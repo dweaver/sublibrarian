3bafa3710b3542e31792db8ea68077e298dfe5c3
danielweaver <daniel@sublibrarian.org>
2009-09-03 15:41:26 -0500

    Changes to be committed:    (use "git rm --cached <file>..." to unstage)
    	new file:   .project
    	new file:   .pydevproject
    	new file:   __init__.py
    	new file:   __init__.pyc
    	new file:   geoticker/__init__.py
    	new file:   geoticker/__init__.pyc
    	new file:   geoticker/models.py
    	new file:   geoticker/tests.py
    	new file:   geoticker/urls.py
    	new file:   geoticker/urls.pyc
    	new file:   geoticker/views.py
    	new file:   geoticker/views.pyc
    	new file:   manage.py
    	new file:   settings.py
    	new file:   settings.pyc
    	new file:   urls.py
    	new file:   urls.pyc
    	new file:   views.py

---
+++ geoticker/views.py
@@ -0,0 +1,19 @@
+# Create your views here.
+from django.http import Http404
+from django.core import serializers
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+import datetime
+import re 
+from django import forms
+
+def index(request):
+    l = [x for x in [1, 2, 3, 4, 5]]
+    
+    return HttpResponse('test, yay! %d' % l[0])
+
+#render_to_response('ouija/index.html',
+#        {'http_host': request.META['SERVER_NAME']}, 
+#        context_instance = RequestContext(request))
