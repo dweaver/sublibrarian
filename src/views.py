0b71c05b2abac76c417afbe9607ca07b9a279fb8
danielweaver <daniel@sublibrarian.org>
2009-09-04 00:38:28 -0500

    Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	new file:   src/__init__.py
    	new file:   src/__init__.pyc
    	new file:   src/geoticker/__init__.py
    	new file:   src/geoticker/__init__.pyc
    	new file:   src/geoticker/models.py
    	new file:   src/geoticker/tests.py
    	new file:   src/geoticker/urls.py
    	new file:   src/geoticker/urls.pyc
    	new file:   src/geoticker/views.py
    	new file:   src/geoticker/views.pyc
    	new file:   src/manage.py
    	new file:   src/settings.py
    	new file:   src/settings.pyc
    	new file:   src/urls.py
    	new file:   src/views.py
    
     Changed but not updated:
       (use "git add/rm <file>..." to update what will be committed)
       (use "git checkout -- <file>..." to discard changes in working directory)
    
    	modified:   .project
    	modified:   .pydevproject
    	deleted:    __init__.py
    	deleted:    __init__.pyc
    	deleted:    geoticker/__init__.py
    	deleted:    geoticker/__init__.pyc
    	deleted:    geoticker/models.py
    	deleted:    geoticker/tests.py
    	deleted:    geoticker/urls.py
    	deleted:    geoticker/urls.pyc
    	deleted:    geoticker/views.py
    	deleted:    geoticker/views.pyc
    	deleted:    manage.py
    	deleted:    settings.py
    	deleted:    settings.pyc
    	deleted:    urls.py
    	deleted:    urls.pyc
    	deleted:    views.py
    	deleted:    views.pyc

---
+++ src/views.py
@@ -0,0 +1,6 @@
+from django.http import Http404
+from django.http import HttpResponse
+from django.shortcuts import render_to_response, get_object_or_404
+
+def index(request):
+    return HttpResponse('test, yay!')
