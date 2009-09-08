54111d03a48af86ae1c69685916c852dd7575680
weaver <weaver@web44.webfaction.com>
2009-09-08 13:54:56 -0500

    Committed with -a. How does this work?

--- views.py
+++
@@ -1,6 +0,0 @@
-from django.http import Http404
-from django.http import HttpResponse
-from django.shortcuts import render_to_response, get_object_or_404
-
-def index(request):
-    return HttpResponse('test, yay!')
