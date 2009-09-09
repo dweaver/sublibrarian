817775bf80cf6c3bfebb12df88406f5184ba8a41
danielweaver <daniel@sublibrarian.org>
2009-09-09 15:04:20 -0500

    mustache splash screen

--- src/views.py
+++ src/views.py
@@ -1,6 +1,9 @@
 from django.http import Http404
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
 
 def index(request):
-    return HttpResponse('test, yay! Hello! Wow!')
+    return render_to_response('index.html', 
+                              {'data': 1},
+                              context_instance=RequestContext(request))
